e11dd93611868104c44f7675685f17363f15254d
Fabian Diartt <fdiartt@technisupport.com>
2019-08-25 06:52:03 -0500

    - control input/output

---
+++ ejercicios/02_control/01_obtener_input.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+main() {
+
+  // imprimir en la terminal o cmd
+  stdout.writeln('¿Cual es tu nombre?');
+
+  // leer informacion
+  String nombre = stdin.readLineSync();
+
+  stdout.writeln( 'Tu nombre es: ' + nombre );
+  stdout.writeln( 'Tu nombre es: $nombre' );
+
+
+}
